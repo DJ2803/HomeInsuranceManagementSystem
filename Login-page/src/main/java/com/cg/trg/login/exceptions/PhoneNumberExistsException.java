4275262046a9d23e64c2db17f672f112c67b0f79
Nikhil Kumar Reddy <70535547+NikhilReddii@users.noreply.github.com>
2021-03-20 09:43:42 +0530

    Delete Login-page directory

--- Login-page/src/main/java/com/cg/trg/login/exceptions/PhoneNumberExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.cg.trg.login.exceptions;
-
-public class PhoneNumberExistsException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public PhoneNumberExistsException(String errorMessage) {
-		super(errorMessage);
-	}
-}
