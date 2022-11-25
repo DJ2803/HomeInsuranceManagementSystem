fd9fe94099d3c2d2e60cc46f8e98bbfd257f8cfb
padmarahul <64130719+padmarahul@users.noreply.github.com>
2022-11-25 13:42:52 +0530

    Delete Frontend/home-insurance/Backend/HomeInsuranceManagementSystem/src directory

--- Frontend/home-insurance/Backend/HomeInsuranceManagementSystem/src/main/java/com/cg/hims/exceptions/PolicyNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.cg.hims.exceptions;
-
-public class PolicyNotFoundException extends Exception {
-
-	public PolicyNotFoundException(String message) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
