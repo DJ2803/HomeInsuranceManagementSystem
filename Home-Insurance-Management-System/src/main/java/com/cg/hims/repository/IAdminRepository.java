7dddbf3a4443dfd14b2ff95b1c6fd27cef21ea29
padmarahul <64130719+padmarahul@users.noreply.github.com>
2022-11-25 13:26:00 +0530

    Delete Home-Insurance-Management-System directory

--- Home-Insurance-Management-System/src/main/java/com/cg/hims/repository/IAdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cg.hims.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.hims.entities.Admin;
-
-public interface IAdminRepository extends JpaRepository<Admin,Integer>{
-
-}
