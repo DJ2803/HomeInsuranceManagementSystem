094b33edf4ce6a0dc7e92505880607bf27c9b9c6
padmarahul <64130719+padmarahul@users.noreply.github.com>
2022-11-25 13:44:33 +0530

    Add files via upload

---
+++ Backend/HomeInsuranceManagementSystem/src/main/java/com/cg/hims/exceptions/AgentNotFoundException.java
@@ -0,0 +1,18 @@
+package com.cg.hims.exceptions;
+
+public class AgentNotFoundException extends Exception {
+	public AgentNotFoundException() {
+
+	}
+
+
+	public AgentNotFoundException(String message) {
+			super(message);
+	}
+
+
+	@Override
+	public String toString() {
+		return "AgentNotFoundException:" + super.getMessage();
+	}
+}
